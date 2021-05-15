2ecfeb11b6c74875d5cee3530f95fe6d653b3a39
Nano112 <harrisonbastian.nano@gmail.com>
2021-05-15 23:53:51 +0100

    cleaned code a bit

--- src/cogs/maincog.py
+++ src/cogs/maincog.py
@@ -15,14 +15,53 @@ class MainCog(commands.Cog):
         self.mute_list = []
 
     async def send_help_embed(self, ctx):
-            pprint(vars(self))
-            pprint(vars(ctx))
             embed = discord.Embed(title="Specialties",  description="redBot can manage your specialties ", color=0xff0000)
             embed.add_field(name="list", value="type **$specialties list** to get the list of specialties",  inline=False)
             embed.add_field(name="get", value="type **$specialties get [specialty1 specialty2 ...]** to get specialties",  inline=False)
             embed.add_field(name="remove", value="type **$specialties remove [specialty1 specialty2 ...]** to remove specialties",  inline=False)
             await ctx.send(embed=embed)
-            
+
+    async def list_specialties(self, ctx):
+        embed = discord.Embed(title="Available specialties", description="These are the specialties you can get", color=0xff0000)
+        for index, specialty in enumerate(self.specialties):
+            embed.add_field(name=index, value='**'+specialty+'**', inline=False)
+        await ctx.send(embed=embed)
+
+    async def get_specialty(self, ctx, params):
+        author = ctx.message.author
+        params = params[1:]
+        added_specialties = []
+        unknown_specialties = []
+        for param in params:
+            param = param.capitalize()
+            if param in self.specialties:
+                added_specialties.append(param)
+                specialty = discord.utils.get(author.guild.roles, name=param)
+                await author.add_roles(specialty)
+            else:
+                unknown_specialties.append(param)
+        await ctx.send(('Roles ' + repr(added_specialties) + ' were added to ' + author.name) if len(
+            added_specialties) > 0 else 'No ranks were added')
+        if len(unknown_specialties) > 0:
+            await ctx.send(('Roles ' + repr(unknown_specialties) + ' are unknown '))
+
+    async def remove_specialty(self, ctx, params):
+        author = ctx.message.author
+        params = params[1:]
+        added_specialties = []
+        unknown_specialties = []
+        for param in params:
+            param = param.capitalize()
+            if param in self.specialties:
+                added_specialties.append(param)
+                specialty = discord.utils.get(author.guild.roles, name=param)
+                await author.remove_roles(specialty)
+            else:
+                unknown_specialties.append(param)
+        await ctx.send(('Roles ' + repr(added_specialties) + ' were removed from ' + author.name) if len(
+            added_specialties) > 0 else 'No ranks were added')
+        if len(unknown_specialties) > 0:
+            await ctx.send(('Roles ' + repr(unknown_specialties) + ' are unknown '))
 
     @commands.command()
     async def specialties(self, ctx, *, message = None):
@@ -30,66 +69,16 @@ class MainCog(commands.Cog):
             await self.send_help_embed(ctx)
             return
         params = message.split()
-        if params[0] == 'list':
-            embed = discord.Embed(title="Available specialties", description="These are the specialties you can get",
-                                  color=0xff0000)
-            for index, specialty in enumerate(self.specialties):
-                embed.add_field(name=index, value='**'+specialty+'**', inline=False)
-            await ctx.send(embed=embed)
-        if params[0] == 'get':
-            params = params[1:]
-            added_specialties = []
-            unknown_specialties = []
-            for param in params:
-                param = param.capitalize()
-                if param in self.specialties:
-                    added_specialties.append(param)
-                    specialty = discord.utils.get(message.author.guild.roles, name=param)
-                    await author.add_roles(specialty)
-                else:
-                    unknown_specialties.append(param)
-            await ctx.send(('Roles ' + repr(added_specialties) + ' were added to ' + author.name) if len(
-                added_specialties) > 0 else 'No ranks were added')
-            if len(unknown_specialties) > 0:
-                await ctx.send(('Roles ' + repr(unknown_specialties) + ' are unknown '))
-        elif params[0] == 'remove':
-            params = params[1:]
-            added_specialties = []
-            unknown_specialties = []
-            for param in params:
-                param = param.capitalize()
-                if param in self.specialties:
-                    added_specialties.append(param)
-                    specialty = discord.utils.get(author.guild.roles, name=param)
-                    await author.remove_roles(specialty)
-                else:
-                    unknown_specialties.append(param)
-            await ctx.send(('Roles ' + repr(added_specialties) + ' were removed from ' + author.name) if len(
-                added_specialties) > 0 else 'No ranks were added')
-            if len(unknown_specialties) > 0:
-                await ctx.send(('Roles ' + repr(unknown_specialties) + ' are unknown '))
+        sub_command = params[0]
+        if sub_command == 'list':
+            await self.list_specialties(ctx)
+            return
+        elif sub_command == 'get':
+            await self.get_specialty(ctx, params)
+            return
+        elif sub_command == 'remove':
+            await self.remove_specialty(ctx, params)
 
-    async def on_message(self, message):
-        content = message.content
-        channel = message.channel
-        author = message.author
-        if author in self.mute_list:
-            self.delete_message(message)
-            return
-        if channel.name != 'bot':
-            return
-        if not content.startswith('$'):
-            return
-        content = content[1:]
-        if author == self.user:
-            return
-
-        print(content)
-        if content.startswith('help'):
-            embed = discord.Embed(title="Commands", description="redBot is here to help", color=0xff0000)
-            embed.add_field(name="specialties", value="type **$specialties** for help about specialties", inline=False)
-            await channel.send(embed=embed)
-           
-
+            
     
 
