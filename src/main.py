9c75a9bd29823c61d8151d8a97ad2574f78b3c90
Zenpai <zenpai@zenpaiang.tk>
2022-07-09 18:30:37 +0800

    formatted code

--- src/main.py
+++ src/main.py
@@ -1,16 +1,15 @@
 
 from discord import AllowedMentions
 from discord.ext import commands
-import os
 from dotenv import load_dotenv
 import json
+import os
 
 load_dotenv()
 token = os.getenv('DISCORD_TOKEN')
 
 with open('config.json') as file:
       config = json.load(file)
-
 
 red_bot = commands.Bot(command_prefix=config["command_prefix"], AllowedMentions = AllowedMentions.none())
 
